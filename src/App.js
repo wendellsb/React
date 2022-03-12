775d638c8fb0fd9e15a495ccc21c641f4e2b02b4
Wendell Santos <wendellsantooos@gmail.com>
2022-03-12 09:25:24 -0300

    Aula 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
 
 function App() {
+
+  const name = 'Wendell'
+
+  const newName = name.toLocaleUpperCase()
+
+  function sum(a, b)  {
+    return a + b
+  }
+
+  const url = "https://via.placeholder.com/150"
+
   return (
     <div className="App">
-      <h1>Olá React</h1>
-      <p>Meu primeiro APP</p>
+      <h1>Alterando o JSX</h1>
+      <p>Olá, { name } </p>
+      <p>Letra maiúscula: {newName} </p>
+      <p>Soma: { 2 + 2 } </p>
+      <p>Soma: {sum(1, 5)} </p>
+      <img src={url} alt="img" />
     </div>
   );
 }
