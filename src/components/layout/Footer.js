9ac84c5f3de7926e2bc2fd41f6926e07a4d8747c
Wendell Santos <wendellsantooos@gmail.com>
2022-03-15 14:05:05 -0300

    Finalizado a parte teórica

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,36 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Footer.module.css'
+
+import { DiAndroid, DiJava, DiJavascript, DiReact, IconName } from 'react-icons/di'
+
 function Footer() {
-    return <footer>Rodapé</footer>
+    return (
+        <footer>
+            <ul className={styles.social_list}>
+                <li>
+                    <FaFacebook />
+                </li>
+                <li>
+                    <FaInstagram /> 
+                </li>
+                <li>
+                    <FaLinkedin />
+                </li>
+                <li>
+                    <DiJavascript />
+                </li>
+                <li>
+                    <DiJava />
+                </li>
+                <li>
+                    <DiReact />
+                </li>
+            </ul>
+            <p>Nosso rodapé</p>
+            
+            
+        </footer>
+    )
 }
 
 export default Footer
